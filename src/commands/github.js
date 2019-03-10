eb211c52c8f7e056fb187d7103c282f945b48bb9
Andrew Jones <andrew@bidops.com>
2019-03-10 13:34:02 -0400

    read maestro.yml

--- src/commands/github.js
+++ src/commands/github.js
@@ -1,10 +1,19 @@
 'use strict';
 const shell = require('shelljs');
+const yaml = require('js-yaml');
+const fs   = require('fs');
 const githubPath = '~/Documents/Github/'
 
 
 module.exports = (args) => {
   const githubProject = args.g;
   shell.cd(`${githubPath}${githubProject}`);
-  console.log(shell.ls());
+
+  try {
+    const config = yaml.safeLoad(fs.readFileSync('./maestro.yml', 'utf8'));
+    console.log(config);
+  }
+  catch (e) {
+    console.log(e);
+  }
 }
